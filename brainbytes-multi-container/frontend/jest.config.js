636a47cc2a5f4d6c791afb6a25ba02a82816ce43
Cate De Guzman <lr.cdguzman@mmdc.mcl.edu.ph>
2025-06-25 19:18:52 +0800

    ...

--- brainbytes-multi-container/frontend/jest.config.js
+++ brainbytes-multi-container/frontend/jest.config.js
@@ -1,6 +1,8 @@
 // jest.config.js
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+
   transform: {
     '^.+\\.[jt]sx?$': 'babel-jest',
   },
